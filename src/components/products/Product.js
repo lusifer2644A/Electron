53b0033c61a7c361c20d5d871a9b9938f99d8e33
lusifer2644A <8083ankit@gmail.com>
2022-02-28 14:08:36 +0530

    Improved Product Pade UI

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,17 +1,23 @@
 import React from "react";
 
 const Product = ({ data }) => {
+    console.log(data);
     return (
         <div className="card">
             <div className="pdImage">
                 <img src={data.url} alt="" />
             </div>
-            <div className="productDetails">
-                <div className="pdCompany large uppercase my-3 heading-color">
-                    {data.company}
+            <div className="pdDetails">
+                <div className="pdDiv1">
+                    <div className="pdName medium dark-grey heading2-color">
+                        {data.name}
+                    </div>
+                    <div className="pdDiscount x-small bold">
+                        {data.discount}% OFF
+                    </div>
                 </div>
-                <div className="pdName medium bold dark-grey heading2-color">
-                    {data.name}
+                <div className="pdCompany small heading-color">
+                    by {data.company}
                 </div>
             </div>
         </div>
